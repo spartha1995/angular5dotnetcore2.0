7e88db57169907cd1b4d6bd4f2356992f0d1ebac
spartha <sarathi1995@live.com>
2018-03-31 11:10:26 +0530

    Initial commit

---
+++ dotnetcoreplusangular5Template/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetcoreplusangular5Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
