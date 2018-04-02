5c5a6d8e08aca2b5d9b691356aaa53630432a0f4
spartha <sarathi1995@live.com>
2018-04-02 10:59:46 +0530

    Added dockerFile config1

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ cd angular5dotnetcore2.0/dotnetcoreplusangular5Template
 dotnet publish -c Release -o published
 
 cd ../../../
-docker build -t spartha1995/angular5dotnetcore2.0:$CIRCLE_BRANCH .
+docker build -t spartha1995/angular5dotnetcore2.0
 docker login -u=$Docker_UserName -p=$Docker_Password
-docker push spartha1995/angular5dotnetcore2.0:$CIRCLE_BRANCH
+docker push spartha1995/angular5dotnetcore2.0
