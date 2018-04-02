6b8c2f8fabc7680079e8ab8445f76410b04a3174
spartha <sarathi1995@live.com>
2018-04-02 12:03:10 +0530

    deploy.sh file modified 1

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ dotnet publish -c Release src
 
 # Build the Docker images
 docker build -t $Docker_Repo:$Tag src/bin/Release/netcoreapp1.0/publish/.
-docker tag $Docker_Repo:$Tag repository/project:latest
+docker tag $Docker_Repo:$Tag $Docker_Repo:latest
 
 # Login to Docker Hub and upload images
 docker login -u="$Docker_UserName" -p="$Docker_Password"
