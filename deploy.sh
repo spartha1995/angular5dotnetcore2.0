322d79eb5969cda69a9f6893c95cc4516ad75bdd
spartha <sarathi1995@live.com>
2018-04-02 13:54:43 +0530

    final

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,8 @@ set -ev
 # Create publish artifact
 dotnet publish -c Release src
 
-# Build the Docker images
-docker build -t $Docker_Repo:$Tag src/bin/Release/angular5dotnetcore2.0/dotnetcoreplusangular5Template/published/.
-docker tag $Docker_Repo:$Tag $Docker_Repo:latest
-
 # Login to Docker Hub and upload images
-docker login -u="$Docker_UserName" -p="$Docker_Password"
-docker push $Docker_Repo:$Tag
-docker push $Docker_Repo:latest
+docker login -u $Docker_UserName -p $Docker_Password
+docker build --no-cache -t $Docker_Repo .
+docker tag $Docker_Repo:latest $Docker_Repo:v1
+docker push $Docker_Repo
