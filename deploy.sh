ce090d129cd37a927017141d8e9a22453913974a
spartha <sarathi1995@live.com>
2018-04-02 12:11:45 +0530

    deploy.sh file modified 2

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -ev
 dotnet publish -c Release src
 
 # Build the Docker images
-docker build -t $Docker_Repo:$Tag src/bin/Release/netcoreapp1.0/publish/.
+docker build -t $Docker_Repo:$Tag src/bin/Release/angular5dotnetcore2.0/dotnetcoreplusangular5Template/published/.
 docker tag $Docker_Repo:$Tag $Docker_Repo:latest
 
 # Login to Docker Hub and upload images
